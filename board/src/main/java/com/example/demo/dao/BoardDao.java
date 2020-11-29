dce2b18095c6418d3ea63a0588ca1a32a90517e4
salt <arcaorm00@gmail.com>
2020-11-29 22:45:01 +0900

    board dao

--- board/src/main/java/com/example/demo/dao/BoardDao.java
+++ board/src/main/java/com/example/demo/dao/BoardDao.java
@@ -1,5 +1,36 @@
 package com.example.demo.dao;
 
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.db.BoardManager;
+import com.example.demo.vo.BoardVo;
+
+@Repository
 public class BoardDao {
 
+	public List<BoardVo> listBoard(){
+		return BoardManager.selectAllBoard();
+	} 
+	
+	public BoardVo getBoard(int id) {
+		return BoardManager.getBoard(id);
+	}
+	
+	public int insertBoard(BoardVo b) {
+		return BoardManager.insertBoard(b);
+	}
+	
+	public int updateBoard(BoardVo b) {
+		return BoardManager.updateBoard(b);
+	}
+	
+	public int plusHit(int id) {
+		return BoardManager.plusHit(id);
+	}
+	
+	public int deleteBoard(int id) {
+		return BoardManager.deleteBoard(id);
+	}
 }
